6a2bca4386ca73eced9b24affb2e5c899ccd0b36
Wendydjoumessi <133278891+wendydjoumessi@users.noreply.github.com>
2023-10-26 17:36:05 +0100

    intergrated axios , nodemon and also display weather of a city

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+import { Oval} from "react-loader-spinner";
+
+function App(prop) {
+
+  function handle(response){
+    alert(`the temperature in ${response.data.name} is ${response.data.main.temp}°C `)
+  }
+  let key = "53f3bc1f5d348c44be3e3754c7185573";
+ 
+  let units = "metric";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${prop.city}&appid=${key}&units=${units}`;
+  axios.get(url).then(handle);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Oval
+    height={80}
+    width={80}
+    color="#4fa94d"
+    wrapperStyle={{}}
+    wrapperClass=""
+    visible={true}
+    ariaLabel='oval-loading'
+    secondaryColor="#4fa94d"
+    strokeWidth={2}
+    strokeWidthSecondary={2}
+      
+    />
   );
 }
 
